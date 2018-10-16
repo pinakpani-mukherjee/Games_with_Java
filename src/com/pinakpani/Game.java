dc1e26c82f78399387d9c164bb6f0aeb597ffa24
pinakpani-mukherjee <mukherjee.pinakpani14@gmail.com>
2018-10-16 23:32:41 +0900

    Added basic comments

--- src/com/pinakpani/Game.java
+++ src/com/pinakpani/Game.java
@@ -2,26 +2,40 @@ package com.pinakpani;
 
 import javax.swing.*;
 import java.awt.*;
-
+/*
+Game class is a daughter of the Canvas class.
+It is also implemeting runnable, with the run function created below
+ */
 public class Game extends Canvas implements Runnable
 {
+    /*
+    Setting up my frames
+     */
     public static int width = 300;
     public static int hight = width/16*9;
     public static int scale = 3;
 
     public Game()
     {
+        /*
+        Constructor for the Game class
+         */
         Dimension size =new Dimension(width*scale,hight*scale);
         setPreferredSize(size);
         frame = new JFrame();
     }
-
+    /*
+    Crating the private variables for the main Game class
+     */
     private Thread gamethread;
     private JFrame frame;
     private boolean running = false;
 
     public synchronized void start()
     {
+        /*
+        Function for game start
+         */
         running = true;
         gamethread = new Thread(this,"Display");
         gamethread.start();
@@ -30,6 +44,9 @@ public class Game extends Canvas implements Runnable
 
     public synchronized void stop()
     {
+        /*
+        Function for game stop
+         */
         running = false;
         try {
             gamethread.join();
@@ -41,6 +58,9 @@ public class Game extends Canvas implements Runnable
 
     public void run()
     {
+        /*
+        this is the main program where we will insert graphics and other stuff
+         */
         while(running)
         {
             System.out.println("Running.....");
@@ -50,6 +70,11 @@ public class Game extends Canvas implements Runnable
 
     public static void main(String[] args)
     {
+        /*
+        Creating my main function
+        remember always put setResizable to false
+        remember always put setDefaultCloseOperation to EXIT_ON_CLOSE
+         */
         Game game = new Game();
         game.frame.setResizable(false);
         game.frame.setTitle("My Game");
@@ -59,6 +84,9 @@ public class Game extends Canvas implements Runnable
         game.frame.setLocationRelativeTo(null);
         game.frame.setVisible(true);
 
+        /*
+        starts the game
+         */
         game.start();
 
     }
