e3e2a9e69583d09ec0bd54ee2c601561b90ceeee
pinakpani-mukherjee <mukherjee.pinakpani14@gmail.com>
2018-10-25 12:03:39 +0900

    added map rendering

--- src/com/pinakpani/graphics/Screen.java
+++ src/com/pinakpani/graphics/Screen.java
@@ -1,21 +1,29 @@
 package com.pinakpani.graphics;
 
+import java.util.Random;
+
 public class Screen
 {
     private int width, height;
     public int[] pixels;
+    public int[] tiles = new int[64*64];
 
-    int xtime = 0;
-    int ytime = 0;
-    int counter = 0;
+    private Random random = new Random();
+
 
     public Screen(int width, int height)
     {
         this.width = width;
         this.height = height;
         pixels = new int[width*height];
+
+        for(int i = 0; i < 64*64;i++)
+        {
+            tiles[i] = random.nextInt(0xffffff);
+
+        }
     }
-    
+
     public void clear()
     {
         for (int i =0; i<pixels.length;i++)
@@ -25,29 +33,24 @@ public class Screen
     }
     public void render()
     {
-        counter++;
-        if(counter%100 == 0)
-        {
-            xtime++;
-        }
-        if(counter%100 == 0)
-        {
-            ytime++;;
-        }
 
         for (int y =0;y<height; y++)
         {
-            if (ytime >= height)
+            if (y >= height)
             {
                 break;
             }
            for(int x = 0; x<width;x++)
            {
-               if (xtime>=width)
+               if (x>=width)
                {
                    break;
                }
-               pixels[xtime+ytime*width] = 0xffffff;
+               // use bitwise operators for faster rendering, always remember to
+               // use this operation in nested continuous for loops
+               int tileIndex = (x >> 4) + (y >> 4)*64;
+
+               pixels[x+y*width] = tiles[tileIndex];
 
            }
         }
